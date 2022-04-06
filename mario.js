a72492ebf941ab1dd604d81f618d77503fe6e0da
Will Gundy <willgunderson503@gmail.com>
2022-04-06 13:15:04 -0700

    changed to package instead of cdn:

--- mario.js
+++ mario.js
@@ -1,9 +1,12 @@
+import kaboom from '../node_modules/kaboom/dist/kaboom.mjs';
+
+
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 2.5, 
+    scale: 2, 
     debug: true,
-    clearColor: [0, 0, 0, 1],
+    background: [0, 0, 0, 1],
 });
 
 //sprites, blocks to build world
@@ -17,20 +20,20 @@ loadSprite('surprise-box', 'assets/surprise-box.png');
 loadSprite('pipe', 'assets/pipe.png');
 
 //sounds to play during gameplay
-loadRoot('https://dazzling-vacherin-8cb912.netlify.app/assets/');
-// loadRoot('http://localhost:5501/assets/');
+// loadRoot('https://dazzling-vacherin-8cb912.netlify.app/assets/');
+loadRoot('http://localhost:5501/assets/');
 loadSound('jump', 'marioJump.mp3');
 loadSound('theme', 'mainTheme.mp3');
 
 
 
-scene('game', () => {
+scene('game', ({ score, count }) => {
     layers(['bg', 'obj', 'ui'], 'obj');
 
     const map = [
         '                                     ',
         '                                     ',
-        '                                     ',
+        '        ***                          ',
         '                                     ',
         '                                     ',
         '                 ****                ',
@@ -47,7 +50,7 @@ scene('game', () => {
     ];
 
     const player = add([
-        sprite('mario'), solid(),
+        sprite('mario'), solid(), area(),
         pos(30, 0),
         body(),
         origin('bot')
@@ -55,16 +58,18 @@ scene('game', () => {
 
     const marioSpeed = 120;
     const marioJumpHeight = 400;
+    const coinScore = 200;
+    const mushroomMove = 20;
 
-    keyDown('left', () => {
+    onKeyDown('left', () => {
         player.move(-marioSpeed, 0);
     });
 
-    keyDown('right', () => {
+    onKeyDown('right', () => {
         player.move(marioSpeed, 0);
     });
 
-    keyPress('space', () => {
+    onKeyPress('space', () => {
         if (player.grounded()) {
             player.jump(marioJumpHeight);
             play('jump');
@@ -74,24 +79,105 @@ scene('game', () => {
     player.on('headbump', (obj) => {
         if (obj.is('coin-surprise')) {
             gameLevel.spawn('*', obj.gridPos.sub(0, 1));
+            gameLevel.spawn('+', obj.gridPos.sub(0, 0));
             destroy(obj);
         }
+        if (obj.is('mushroom-surprise')) {
+            gameLevel.spawn('@', obj.gridPos.sub(0, 1));
+            gameLevel.spawn('+', obj.gridPos.sub(0, 0));
+            destroy(obj);
+        }
+    });
+
+    action('mushroom', (e) => {
+        e.move(mushroomMove, 0);
+    });
+
+    action('evil-mushroom', (e) => {
+        e.move(-mushroomMove, 0);
+    });
+
+    // player.collides('mushroom', (e) => {
+    //     destroy(e);
+    //     player.biggify(10);
+    // });
+
+    player.collides('coin', (e) => {
+        destroy(e);
+        scoreLabel.value += coinScore;
+        scoreLabel.text = scoreLabel.value;
+        console.log(score);
     });
 
     const levelConfig = {
         width: 20,
         height: 20,
-        '=': [sprite('brick'), solid()],
-        '*': [sprite('coin')],
-        '%': [sprite('surprise-box'), solid(), 'coin-surprise'],
-        '#': [sprite('surprise-box'), solid(), 'mushroom-surprise'],
-        '^': [sprite('evil-mushroom'), solid()],
-        '?': [sprite('pipe'), solid()],
-        '+': [sprite('block'), solid()],
+        '=': [sprite('brick'), solid(), area()],
+        '*': [sprite('coin'), 'coin'],
+        '%': [sprite('surprise-box'), solid(), area(), 'coin-surprise'],
+        '#': [sprite('surprise-box'), solid(), area(), 'mushroom-surprise'],
+        '^': [sprite('evil-mushroom'), solid(), area(), 'evil-mushroom', body()],
+        '?': [sprite('pipe'), solid(), area()],
+        '+': [sprite('block'), solid(), area()],
+        '@': [sprite('mushroom'), solid(), area(), 'mushroom', body()],
     };
 
-    play('theme');
-    const gameLevel = addLevel(map, levelConfig);
+    // play('theme');
+
+    const username = add([
+        text('MARIO'),
+        pos(30, 6),
+    ]);
+    const scoreLabel = add([
+        text(score),
+        pos(60, 20),
+        layer('ui'),
+        {
+            value: score
+        }
+    ]);
+    const coinCount = add([
+        text(sprite('coin') + 'x' + count),
+        pos(100, 20),
+        layer('ui'),
+        {
+            value: count
+        }
+    ]);
+
+    const gameLevel = addLevel([
+        "                          $",
+        "                          $",
+        "           $$         =   $",
+        "  %      ====         =   $",
+        "                      =    ",
+        "       ^^      = >    =   &",
+        "===========================",
+    ], {
+        // define the size of each block
+        width: 32,
+        height: 32,
+        // define what each symbol means, by a function returning a component list (what will be passed to add())
+        "=": () => [
+            sprite('block'),
+            area(),
+            solid(),
+        ],
+        "$": () => [
+            sprite('coin'),
+            area(),
+            pos(0, -9),
+        ],
+        "^": () => [
+            sprite('mushroom'),
+            area(),
+            "danger",
+        ],
+    });
+
+    player.action(() => {
+        camPos(player.pos);
+    });
 });
 
-start('game');
+go('game', { score: 0, count: 0 });
